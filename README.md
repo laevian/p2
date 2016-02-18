3a6fe66a4ede025ad87a94fd8f9d8c6433fea92b
laevian <lpocklin@gmail.com>
2016-02-18 10:42:32 -0500

    Updated comments, created README

---
+++ README.md
@@ -0,0 +1,17 @@
+#XKCD Password Generator
+
+##Live Project
+
+[p2.lpocklin.me](p2.lpocklin.me)
+
+##Project Description
+
+A password generator based on [this comic](https://xkcd.com/936/) from XKCD. The generator scrapes the table of words from the Connecticut State Community College's list of most commonly misspelled words located [here](http://www.ccc.commnet.edu/grammar/misspelled_words.htm) , cleans the data, and then picks a set of words and outputs it as a string separated by -. The number of words and whether the password includes a symbol and/or a number is determined by the user's input; by default the generator picks a four-word password without symbols or numbers.
+
+##Demo
+
+Will be updated when available.
+
+##Outside Code
+
+CCC's [Most Commonly Misspelled Words Page](http://www.ccc.commnet.edu/grammar/misspelled_words.htm)
--- logic.php
+++ logic.php
@@ -1,75 +1,70 @@
 <?php
 
-/*
-The following code scrapes the page at http://www.ccc.commnet.edu/grammar/misspelled_words.htm for the table of words it contains, cleans the data, and then generates a password using four words picked at random from the array of words.
+	/*
+	The following code scrapes the page at http://www.ccc.commnet.edu/grammar/misspelled_words.htm for the table of words it contains, cleans the data, and then generates a password using four words picked at random from the array of words.
 
-*/
+	*/
 
-$rawdata = file_get_contents("http://www.ccc.commnet.edu/grammar/misspelled_words.htm");
+	$rawdata = file_get_contents("http://www.ccc.commnet.edu/grammar/misspelled_words.htm");
 
-$dom = new domDocument; 
+	$dom = new domDocument; 
 
-@$dom->loadHTML($rawdata); # loads the HTML into the domDocument
+	@$dom->loadHTML($rawdata); # loads the HTML into the domDocument
 
-$tables = $dom->getElementsByTagName('table'); # grabs all of the tables from the HTML
+	$tables = $dom->getElementsByTagName('table'); # grabs all of the tables from the HTML
 
-foreach ($tables as $table => $data) { # cycles through $tables array until it gets to the one I want, and then dumps the values into $wordTable.
-	
-	$wordTable = $tables[3];
-    # echo $data->nodeValue, PHP_EOL; # on each cycle, outputs the data from the processed table
-}
+	foreach ($tables as $table => $data) { # cycles through $tables to pull out the word table
+		$wordTable = $tables[3];
+	}
 
-$wordList = $wordTable->nodeValue; # takes the entire set of words from DOMElement wordTable, makes it new string wordList.
-$words = explode(" ", $wordList); # turns wordList into an array.
-$wordCount = count($words);
+	$wordList = $wordTable->nodeValue; # dumps DOMElement data into a string
+	$words = explode(" ", $wordList); # dumps string data into an array
+	$wordCount = count($words); #upper limit for rand
 
-		$error = NULL;
-		$password = "";
-		$numOfWords = $_GET["numOfWords"];
-	
-	if ($_GET["numOfWords"] == NULL)
-	{
+	$error = NULL; #resets the error, if it has been set before
+	$password = "";
+	$numOfWords = $_GET["numOfWords"];
+		
+	if ($_GET["numOfWords"] == NULL){ #Initial page load case
 		$numOfWords = 4;
 	}
-	
-	else if ($_GET["numOfWords"] == "0" OR is_numeric($_GET["numOfWords"]) == false)
-	{
+		
+	else if ($_GET["numOfWords"] == "0" OR is_numeric($_GET["numOfWords"]) == false){ #error catching
 		$error = "The Number of Words input field only accepts numbers between 1 and 9. Please change your input and try again.";
 		$numOfWords = 4;
 	}
 
 
-	for ($i = 0; $i < $numOfWords; $i++)
+	for ($i = 0; $i < $numOfWords; $i++) #loop to process user input
 	{
 		
-		if ($i == 0)
+		if ($i == 0) #sets the first word (no - needed), then proceeds to the else if
 		{
-			$randWord = rand(0, $wordCount-1); #generates a number 
+			$randWord = rand(0, $wordCount-1);
 			$wordChoice = $words[$randWord];
 			$password = $wordChoice;
 		}
-		
-		else if ($i > 0)
+			
+		else if ($i > 0)#concats password thus far with -[new word]
 		{
-			$randWord = rand(0, $wordCount-1); #generates a number 
+			$randWord = rand(0, $wordCount-1);
 			$wordChoice = $words[$randWord];
-			$password = $password."-".$wordChoice;	
+			$password = $password."-".$wordChoice;
 		}
 	}
-	
-	if (isset($_GET["symbol"])){
+		
+	if (isset($_GET["symbol"])){ #concats password with a symbol if checkbox is ticked
 		$symbolArray = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="];
 		$randSymbol = rand(0, 13);
 		$symbolChoice = $symbolArray[$randSymbol];
 		$password = $password."-".$symbolChoice;
-		
+			
 	}
-	
-	if (isset($_GET["number"])){
+		
+	if (isset($_GET["number"])){ #concats password with a number if checkbox is ticked
 		$randNumber = rand(0, 9);
 		$password = $password."-".$randNumber;
 	}
-	
-	return $password;
+		
 	
 ?>
